268e0bf7f511000e0dcaad583923044803999c63
abuyck98 <30398038+abuyck98@users.noreply.github.com>
2017-07-26 13:30:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-//console.log("what up");
-//console.log(2 + 2);
+console.log("what up");
+console.log(2 + 2);
 
-function checkout (item1, item2, coupon) {
+function checkout (item1, item2, coupon, salesTax) {
   var subtotal = item1 + item2;
+  var salesTax = .0725;
   var couponValue = subtotal * coupon;
   var grandTotal = subtotal - couponValue;
-  //console.log(grandTotal);
-  return grandTotal;
+  console.log(grandTotal);
+  //return grandTotal;
 }
 
-console.log(checkout(33, 19, .15));
+checkout(50, 25, .25, .0725);
 
-var bananaPrice = 1.5;
-var pudding = 6;
-var coupon = .2;
+//var bananaPrice = 1.5;
+//var pudding = 6;
+//var bankAccount = 350;
 
-var bankAccount = 350;
+//var remainAmount = bankAccount - checkout(bananaPrice, pudding, coupon);
 
-var remainAmount = bankAccount - checkout(bananaPrice, pudding, coupon);
+//console.log("Total amount left in bank: " + remainAmount);
 
-console.log("Total amount left in bank: " + remainAmount);
+var test = $('#purple-colored-heading');
+test.text('I hate this stuff');
+
+var elevatorLine = [];
+
+function pressFloorNumber (elevatorLine, floorPressed) {
+  elevatorLine.push(floorPressed);
+  console.log ("position " + elevatorLine.length);
+}
+
+function goToNextFloor () {
+  if (elevatorLine.length == 0) console.log ("There are no more floors to go to!");
+  else {
+    console.log(elevatorLine[0])
+    elevatorLine.splice(0, 1);
+  }
+}
+
+function currentLine (elevatorLine){
+  var index = 0;
+  while(index < elevatorLine.length) {
+    console.log(" floor " + elevatorLine[index])
+    index++;
+  }
+}
